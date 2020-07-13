1e9dacbe30c61b15983c082e2d01386c4a601798
Jung <junghong91@naver.com>
2020-07-13 18:38:11 +0900

    #5.1 Reservations Model

--- reviews/models.py
+++ reviews/models.py
@@ -14,7 +14,7 @@ class Review(core_models.TimeStampedModel):
     check_in = models.IntegerField()
     value = models.IntegerField()
     user = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    rooms = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.review} - {self.rooms}"
+        return f"{self.review} - {self.room}"
