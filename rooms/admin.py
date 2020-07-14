49d85428574a60f5c4383cd0cb72c12c6176f85c
Jung <junghong91@naver.com>
2020-07-15 01:05:28 +0900

    #7.3 Finishing the Room Admin, except for the Photo

--- rooms/admin.py
+++ rooms/admin.py
@@ -7,6 +7,11 @@ class ItemAdmin(admin.ModelAdmin):
 
     """ Item Admin Definition """
 
+    list_display = ("name", "used_by")
+
+    def used_by(self, obj):
+        return obj.rooms.count()
+
     pass
 
 
@@ -45,6 +50,7 @@ class RoomAdmin(admin.ModelAdmin):
         "check_out",
         "instant_book",
         "count_amenities",
+        "count_photos",
     )
 
     list_filter = (
@@ -75,6 +81,9 @@ class RoomAdmin(admin.ModelAdmin):
         return obj.amenities.count()
 
     count_amenities.short_description = "Amenity Count"
+
+    def count_photos(self, obj):
+        return obj.photos.count()
 
 
 @admin.register(models.Photo)
