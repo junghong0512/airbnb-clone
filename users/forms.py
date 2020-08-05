e5a5d7f88d6f08f3a8de3602d051655a5845e719
Jung <junghong91@naver.com>
2020-08-05 11:14:38 +0900

    #14.3 Validating Password, and returning the cleaned_data by using clean method

--- users/forms.py
+++ users/forms.py
@@ -7,14 +7,36 @@ class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
 
-    def clean_email(self):
+    def clean(self):
         email = self.cleaned_data.get("email")
-        print(email)
+        password = self.cleaned_data.get("password")
         try:
-            models.User.objects.get(username=email)
-            return email
+            user = models.User.objects.get(email=email)
+            if user.check_password(password):
+                return self.cleaned_data
+            else:
+                # raise forms.ValidationError("Password is Wrong")
+                self.add_error("password", forms.ValidationError("Password is Wrong"))
         except models.User.DoesNotExist:
-            raise forms.ValidationError("User Does Not Exist")
+            # raise forms.ValidationError("User is not exist")
+            self.add_error("email", forms.ValidationError("User is not exist"))
 
-    def clean_password(self):
-        return "cleaned password"
+    # def clean_email(self):
+    #     email = self.cleaned_data.get("email")
+    #     try:
+    #         models.User.objects.get(username=email)
+    #         return email
+    #     except models.User.DoesNotExist:
+    #         raise forms.ValidationError("User Does Not Exist")
+
+    # def clean_password(self):
+    #     email = self.cleaned_data.get("email")
+    #     password = self.cleaned_data.get("password")
+    #     try:
+    #         user = models.User.objects.get(username=email)
+    #         if user.check_password(password):
+    #             return password
+    #         else:
+    #             raise forms.ValidationError("Password is Wrong")
+    #     except models.User.DoesNotExist:
+    #         pass
