c58e126384158a2bec7de4a5a87fa313e3d7cf30
Jung <junghong91@naver.com>
2020-08-05 17:18:51 +0900

    #15.0 SignUp Form, Using FormView

--- users/forms.py
+++ users/forms.py
@@ -40,3 +40,28 @@ class LoginForm(forms.Form):
     #             raise forms.ValidationError("Password is Wrong")
     #     except models.User.DoesNotExist:
     #         pass
+
+
+class SignUpForm(forms.Form):
+
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            user = models.User.objects.get(email=email)
+            raise forms.ValidationError("Usern already exists with that email")
+        except models.User.DoesNotExist:
+            return email
+
+    def clean_password1(self):
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+
+        if password != password1:
+            raise forms.ValidationError("The password confirmation does not match")
+            return password
--- users/views.py
+++ users/views.py
@@ -41,3 +41,14 @@ class LoginView(FormView):
 def logout_view(request):
     logout(request)
     return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {
+        "first_name": "Jung",
+        "last_name": "Hong",
+        "email": "junghong91@naver.com",
+    }
