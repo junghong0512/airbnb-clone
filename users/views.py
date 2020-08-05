cb53ef97d549b64dd7390d874bd298d4a3efd4f5
Jung <junghong91@naver.com>
2020-08-05 15:00:17 +0900

    #14.5 Always an Easier Way. Using an AuthenticationForm, FormView

--- users/views.py
+++ users/views.py
@@ -1,11 +1,28 @@
 from django.views import View
+from django.views.generic import FormView
+from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
-class LoginView(View):
-    def get(self, request):
+class LoginView(FormView):
+
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
+    initial = {"email": "junghong0512@gmail.com"}
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+
+
+"""     def get(self, request):
         form = forms.LoginForm(initial={"email": "junghong0512@gmail.com"})
         return render(request, "users/login.html", {"form": form})
 
@@ -18,7 +35,7 @@ class LoginView(View):
             if user is not None:
                 login(request, user)
                 return redirect(reverse("core:home"))
-        return render(request, "users/login.html", {"form": form})
+        return render(request, "users/login.html", {"form": form}) """
 
 
 def logout_view(request):
