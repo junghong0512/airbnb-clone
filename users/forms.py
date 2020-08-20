1bc6fbd7832d70de7d1535faf37a21b1853d4554
Jung <junghong91@naver.com>
2020-08-20 16:19:13 +0900

    #20.11 Forms Shortcut, Using for loop and django field

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from . import models
 
 
@@ -24,26 +23,6 @@ class LoginForm(forms.Form):
             # raise forms.ValidationError("User is not exist")
             self.add_error("email", forms.ValidationError("User is not exist"))
 
-    # def clean_email(self):
-    #     email = self.cleaned_data.get("email")
-    #     try:
-    #         models.User.objects.get(username=email)
-    #         return email
-    #     except models.User.DoesNotExist:
-    #         raise forms.ValidationError("User Does Not Exist")
-
-    # def clean_password(self):
-    #     email = self.cleaned_data.get("email")
-    #     password = self.cleaned_data.get("password")
-    #     try:
-    #         user = models.User.objects.get(username=email)
-    #         if user.check_password(password):
-    #             return password
-    #         else:
-    #             raise forms.ValidationError("Password is Wrong")
-    #     except models.User.DoesNotExist:
-    #         pass
-
 
 class SignUpForm(forms.ModelForm):
     class Meta:
@@ -62,6 +41,16 @@ class SignUpForm(forms.ModelForm):
         widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"})
     )
 
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(email=email)
+            raise forms.ValidationError(
+                "User already exists with that email", code="existing_user"
+            )
+        except models.User.DoesNotExist:
+            return email
+
     def clean_password1(self):
         password = self.cleaned_data.get("password")
         password1 = self.cleaned_data.get("password1")
@@ -77,43 +66,3 @@ class SignUpForm(forms.ModelForm):
         user.username = email
         user.set_password(password)
         user.save()
-
-
-# class SignUpForm(UserCreationForm):
-
-#     username = forms.EmailField(label="Email")
-
-
-""" class SignUpForm(forms.Form):
-
-    first_name = forms.CharField(max_length=50)
-    last_name = forms.CharField(max_length=50)
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
-    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
-
-    def clean_email(self):
-        email = self.cleaned_data.get("email")
-        try:
-            models.User.objects.get(email=email)
-            raise forms.ValidationError("Usern already exists with that email")
-        except models.User.DoesNotExist:
-            return email
-
-    def clean_password1(self):
-        password = self.cleaned_data.get("password")
-        password1 = self.cleaned_data.get("password1")
-
-        if password != password1:
-            raise forms.ValidationError("The password confirmation does not match")
-            return password
-
-    def save(self):
-        first_name = self.cleaned_data.get("first_name")
-        last_name = self.cleaned_data.get("last_name")
-        email = self.cleaned_data.get("email")
-        password = self.cleaned_data.get("password")
-        user = models.User.objects.create_user(email, email, password)
-        user.first_name = first_name
-        user.last_name = last_name
-        user.save() """
